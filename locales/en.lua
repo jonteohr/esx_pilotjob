1b31c1b60ff030c645597fff05b144a8d78236a3
Jonathan Öhrström <tacco_9@hotmail.com>
2018-03-09 13:56:15 +0100

    translation fixes

--- locales/en.lua
+++ locales/en.lua
@@ -1,3 +1,13 @@
 Locales ['en'] = {
-	
+	['clothes_hint']					= 'press ~INPUT_CONTEXT~ to change clothes',
+	['vehicle_hint']					= 'press ~INPUT_CONTEXT~ to open garage',
+	['job_hint']						= 'press ~INPUT_CONTEXT~ to open the menu',
+	['create_invoice']					= 'create invoice',
+	['jobmenu_title']					= 'jobmenu',
+	['invoice_amount']					= 'invoice amount',
+	['airline']							= 'Scandinavian Airlines',
+	['vehicle_spawner']					= 'airplanegarage',
+	['civ_clothes']						= 'civilian clothes',
+	['pilot_uniform']					= 'pilotuniform',
+	['clothes_menu']					= 'locker room',
 }
